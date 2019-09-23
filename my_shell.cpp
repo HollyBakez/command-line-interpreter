393495562c0172e51709ac45982a806d2b5fea43
HollyBakez <ho.holland1999@gmail.com>
2019-09-22 23:52:41 -0700

    test file for reference added

--- my_shell.cpp
+++ my_shell.cpp
@@ -2,13 +2,15 @@
 // Copyright(c) Holland Ho 9/14/19
 
 #include <iostream>
+#include <stdio.h>
+#include <windows.h>
 
 using namespace std;
-/* 
+/*
 ----- pseudo code -----
 1. Create infinite loop that calls for a user input or "command"
 2. checks the command with a list of supported commands
-        - dir 
+        - dir
         - help
         - vol
         - path
@@ -18,24 +20,33 @@ using namespace std;
         - color
         - ping
 3. check command and if needed parse user input
-4. execute in child thread via CreateThread() -> executes the command 
+4. execute in child thread via CreateThread() -> executes the command
 5. Parent thread waits for "exit" or "quit" -> terminates shell
-
-
-
 */
 
-int main(){
-    //std::cout<< "hello world";
-    string user_command; 
 
-    while(1){ 
-        cout<<"test \n";
-        cout<<"type 'stop' to exit\n";
-        getline(cin,user_command);
-        if(user_command == "stop"){
-            break;
-        }
+/* the thread will handle the execution of the command*/
+DWORD WINAPI ExecuteCommand(LPVOID command){
+  /* insert code here */
+}
+
+/* compares "command" to the list of supported commands */
+string GetCommand(string command){
+  if (command == "dir"){
+    return system("dir");
+  }
+
+  return 0;
+}
+
+int main(){
+    DWORD thread_id;
+    HANDLE thread_handle;
+    string user_command;
+
+    while(1){
+      /* insert code here */
+
     }
 
     return 0;
