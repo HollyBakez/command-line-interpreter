91b8c4862dab884ba1692d4b64b03156f3993c55
HollyBakez <ho.holland1999@gmail.com>
2019-09-25 23:37:52 -0700

    working

--- my_shell.cpp
+++ my_shell.cpp
@@ -1,114 +1,83 @@
-// my_shell.cpp : Simple Command Line Interpreter
-// Copyright(c) Holland Ho 9/14/19
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
-#include <windows.h>
+#include <pthread.h>
 #include <cstring> 
-
+#include <vector>
 using namespace std;
-/*
------ pseudo code -----
-1. Create infinite loop that calls for a user input or "command"
-2. checks the command with a list of supported commands
-        - dir
-        - help
-        - vol
-        - path
-        - tasklist
-        - notepad
-        - echo
-        - color
-        - ping
-3. check command and if needed parse user input
-4. execute in child thread via CreateThread() -> executes the command
-5. Parent thread waits for "exit" or "quit" -> terminates shell
-*/
-string command_check(string shell_command){
-  string foo = "this doesn't work";
-  string goo = "this works";
 
-  if (shell_command == "dir"){
-    return goo;
-  }
 
-  return foo; 
+void* ExecuteRunner(void* arg){
+    
+    system((char*) arg);
+    pthread_exit(0);
 }
 
-DWORD WINAPI MyThreadFunction(LPVOID Param);
-
 int main(){
-/* delim to check for command arguments amount */
     char delim[] = " ";
-/* user inputs can be compared to the list of support commands */
-    string com_list[3] = {"dir","ping","echo"};
+    /* thread id */
+    pthread_t tid;
 
-    cout<< "-----Welcome to MyShell----- \n";
+    /* create attributes */
+    pthread_attr_t attr;
+    pthread_attr_init(&attr);
+    string user_command; 
+    vector<string> com_list;
+    com_list.push_back("ls");
+    com_list.push_back("ping");
+    com_list.push_back("echo");
+    com_list.push_back("help");
+    com_list.push_back("color");
+    com_list.push_back("notepad");
+    com_list.push_back("tasklist");
+    com_list.push_back("path");
+    com_list.push_back("vol");
 
-    while(1){
 
-      cout<< "-----Please enter a command----- \n";
-      string user_command;
-      getline(cin, user_command);
-      if (user_command == "exit"){
-        cout << "exiting shell..... Thank you for using me! \n"; 
-        break;
-    }
-/* converts user command to char to be parsed */
-      char cstr[user_command.size() + 1];
+                             /*
+    string com_list[9] = {"ls", "ping", "echo", 
+                            "help", "color", "notepad", "tasklist", 
+                             "path", "vol"};
+                             */
+    cout << "---- Welcome to MyShell ---- \n";
 
-/* token should be the strings prior to whitespaces indicating seperate arguments */
-      char *token = strtok(strcpy(cstr, user_command.c_str()), delim);
+    while (1){
+        bool com_exist = false;
+        cout << "---- To exit, enter 'exit' ---- \n";
+        cout << "---- Please enter a command ---- \n";
+        getline(cin, user_command);
 
-      while(token){
-      // ex. ping foo 
-      // token[0] = ping, token[1] = foo
-        //cout << token << endl;
-
-      /* checks dir command */
-        if (token == com_list[0]){
-          system(token);
-        }
-
-      /* checks ping command */
-        if (token == com_list[1]){
-          cout << token << endl;
-          char* first_arg = token;
-          token = strtok(NULL, delim);
-          // checks to see if there are enough arguments
-          if(token == NULL){
-            cout<<"invalid arguments, please try again... \n";
+        if (user_command == "exit" || user_command == "Exit"){
+            cout << "Thank you for using me! \n";
             break;
-          }
-          string combine = first_arg;
-          combine.append(" ");
-          combine.append(token);
-          system(combine.c_str());
+        }
+    /* converts user command into char */
+        char cstr[user_command.size() + 1];
+    /* token should be strings between each whitespace to indicate argument size */
+        char* token = strtok(strcpy(cstr, user_command.c_str()), delim);
+    
+        if (token){
+            // check if the command is supported 
+            for(int i = 0; i < com_list.size() ; i++){ // change sizeof(com_list)  to 8
+                if (token == com_list[i]){
+                    com_exist = true;
+
+                    break;
+                }
+
+            }
+            /* if the command exists, create thread and execute command */
+            if(com_exist == true){
+            strcpy(cstr, user_command.c_str());
+            pthread_create(&tid, &attr, ExecuteRunner, cstr);  
+            pthread_join(tid, NULL);
+            }
+            else{
+                cout << "command is not supported \n";
+            }
         
         }
-
-      /* checks for echo note: assumption that arguments do not contain spaces  ie "hello world"*/
-        if (token == com_list[2]){ 
-          cout << token << endl;
-          char* first_arg = token;
-          token = strtok(NULL, delim);
-          // if only one argument is given
-          if(token == NULL){
-            system(first_arg);
-            break;
-          }
-          else{
-            string combine = first_arg;
-            cout << token << endl;
-            combine.append(" ");
-            combine.append(token);
-            system(combine.c_str());
-          }
-      
-        }
-
-        token = strtok(NULL, delim);
+     
     }
-  }
-    return 0;
+    return 0 ;
 }
